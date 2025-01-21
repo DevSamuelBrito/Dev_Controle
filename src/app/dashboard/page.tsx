97ae07cead83d5b05f61953944f04c06aec806fb
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-01-21 11:49:30 -0300

    criando o componente Container e a pagina Dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/components/container";
+
+const Dashboard = () => {
+    return (
+        <Container>
+            Hellow world!
+        </Container>
+    );
+}
+
+export default Dashboard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import Image from "next/image";
+import heroImag from "@/app/assets/hero.svg";
 
 export default function Home() {
   return (
-       <main>teste</main>
+    <main className="flex  flex-col items-center justify-center min-h-[calc(100vh-80px)]">
+      <h2 className="font-medium text-2xl mb-2">Gerencie sua Empresa</h2>
+
+      <h2 className="font-bold text-3xl mb-8 text-blue-600 md:text-4xl">Atendimentos, clientes</h2>
+      <Image
+        src={heroImag}
+        alt="Imagem do Hero Dev Controle"
+        width={600}
+        className="max-w-sm md:max-w-xl"
+      />
+    </main>
   );
 }
