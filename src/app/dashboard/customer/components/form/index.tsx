26ad026e7c2173ce71499702bd09b42fbbb13abf
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-01-28 17:55:20 -0300

    criando a pagina de criacao de novos clientes

---
+++ src/app/dashboard/customer/components/form/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+export function NewCostumerForm() {
+  return (
+    <form>
+      <label>Nome Completo:</label>
+      <input type="text" placeholder="Nome Completo..." />
+    </form>
+  );
+}
--- src/app/dashboard/customer/new/page.tsx
+++ src/app/dashboard/customer/new/page.tsx
@@ -1,6 +1,16 @@
 import { Container } from "@/components/container";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function Customer() {
+export default async function NewCustomer() {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+
+  if (!session || !session.user) {
+    redirect("/");
+  }
   return (
     <Container>
       <div>pagina de novos clientes</div>
