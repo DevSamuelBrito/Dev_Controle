5f50c71883ea678f800ffb05d01a70f615d2b27c
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-01-28 17:31:05 -0300

    criando o template da pagina de customers no projeto

---
+++ src/app/dashboard/customer/new/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/container";
+
+export default function Customer() {
+  return (
+    <Container>
+      <div>pagina de novos clientes</div>
+    </Container>
+  );
+}
--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -1,6 +1,9 @@
+import { Container } from "@/components/container";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
+import Link from "next/link";
 import { redirect } from "next/navigation";
+import { CardCustomer } from "@/app/dashboard/customer/components/card";
 
 export default async function Customer() {
   const session = await getServerSession(authOptions);
@@ -9,5 +12,24 @@ export default async function Customer() {
   if (!session || !session.user) {
     redirect("/");
   }
-  return <div>Pagina de clientes</div>;
+  return (
+    <Container>
+      <main className="mt-9 mb-2">
+        <div className="flex item-center justify-between">
+          <h1 className="text-3xl font-bold">Meus Clientes</h1>
+          <Link
+            href="/dashboard/customer/new"
+            className="bg-blue-500 text-white px-4 py-1 rounded"
+          >
+            Novo Cliente
+          </Link>
+        </div>
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+          <CardCustomer />
+          <CardCustomer />
+          <CardCustomer />
+        </section>
+      </main>
+    </Container>
+  );
 }
