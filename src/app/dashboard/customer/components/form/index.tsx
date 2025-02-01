9d9d15c38500775f1d44c08350277f9c6fb6b6d4
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-02-01 19:34:25 -0300

    adicionando validação de email, telefone e endereço no formulário de novos clientes

--- src/app/dashboard/customer/components/form/index.tsx
+++ src/app/dashboard/customer/components/form/index.tsx
@@ -4,16 +4,35 @@ import { useForm } from "react-hook-form";
 import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-
 const schema = z.object({
-  name:z.string().min(1, "campo é obrigatorio"),
-})
+  name: z.string().min(1, "campo é obrigatorio"),
+  email: z
+    .string()
+    .email("Digite um email valido")
+    .min(1, "o campo é obrigatorio"),
+  phone: z.string().refine(
+    (value) => {
+      return (
+        /^(?:\(\d{2}\)\s?)?\d{9}$/.test(value) ||
+        /^\d{2}\s\d{9}$/.test(value) ||
+        /^\d{11}$/.test(value)
+      );
+    },
+    { message: "O numero de telefone deve estar (17) 123456789" }
+  ),
+  address: z.string().min(1, "campo é obrigatorio"),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export function NewCostumerForm() {
-
-  const {} = useForm({
-    resolver: zodResolver(schema)
-  })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
 
   return (
     <form>
