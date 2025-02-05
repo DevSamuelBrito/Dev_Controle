fb217434daa474cafa22da8ba64011c24dca0a10
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-02-05 17:52:10 -0300

    fazendo a leitura de cliente

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -1,34 +1,10 @@
+import NextAuth from "next-auth";
+import { AuthOptions } from "next-auth";
+import { NextResponse } from "next/server";
 
-import { NextResponse } from 'next/server'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/lib/auth'
-import prismaClient from '@/lib/prisma'
+export async function POST(request: Request) {
+  const { name, email, phone, address } = await request.json();
 
-
-export async function POST(request: Request){
-  const session = await getServerSession(authOptions);
-
-  if(!session || !session.user){
-    return NextResponse.json({ error: "Not authorized" }, { status: 401 })
-  }
-
-  const { name, email, phone, address, userId } = await request.json();
-
-  try{
-    await prismaClient.customer.create({
-      data:{
-        name,
-        phone,
-        email,
-        address: address ? address : "",
-        userId: userId
-      }
-    })
-
-    return NextResponse.json({ message: "Cliente cadastrado com sucesso!" })
-
-  }catch(err){
-    return NextResponse.json({ error: "Failed crete new customer" }, { status: 400 })
-  }
-
+  console.log(name, email, phone, address);
+  return NextResponse.json({ message: "Post criado" });
 }
--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -1,38 +1,44 @@
-import { Container } from '@/components/container'
-import { authOptions } from '@/lib/auth'
-import { getServerSession } from 'next-auth'
-import { redirect } from 'next/navigation'
+import { Container } from "@/components/container";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import prismaClient from "@/lib/prisma";
 
-import Link from 'next/link'
-import { CardCustomer } from './components/card'
+import Link from "next/link";
+import { CardCustomer } from "./components/card";
 
 export default async function Customer() {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
   if (!session || !session.user) {
-    redirect("/")
+    redirect("/");
   }
 
+  const customers = await prismaClient.customer.findMany({
+    where: {
+      userId: session.user.id,
+    },
+  });
+  console.log(customers);
   return (
     <Container>
       <main className="mt-9 mb-2">
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold">Meus clientes</h1>
-          <Link href="/dashboard/customer/new" className="bg-blue-500 text-white px-4 py-1 rounded">
+          <Link
+            href="/dashboard/customer/new"
+            className="bg-blue-500 text-white px-4 py-1 rounded"
+          >
             Novo cliente
           </Link>
         </div>
 
         <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-2">
-          <CardCustomer />
-
-          <CardCustomer />
-
-          <CardCustomer />
+          {customers.map((customer) => {
+            return <CardCustomer key={customer.id} customer={customer}/>;
+          })}
         </section>
-
-
       </main>
     </Container>
-  )
+  );
 }
