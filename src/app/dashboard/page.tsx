8aed830899a8d7ca287dbc43d5586f7e3542bd70
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-02-06 15:03:16 -0300

    listando tickets na pagina de dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,47 +1,62 @@
-import { Container } from '@/components/container'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/lib/auth'
-import { redirect } from 'next/navigation'
-import Link from 'next/link'
-import { TicketItem } from '@/app/dashboard/components/ticket'
+import { Container } from "@/components/container";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import { TicketItem } from "@/app/dashboard/components/ticket";
+import prismaClient from "@/lib/prisma";
 
 export default async function Dashboard() {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
   if (!session || !session.user) {
-    redirect("/")
+    redirect("/");
   }
-
+  const tickets = await prismaClient.ticket.findMany({
+    where: {
+      userId: session?.user.id,
+      status: "ABERTO",
+    },
+    include: {
+      customer: true,
+    },
+  });
 
   return (
     <Container>
       <main className="mt-9 mb-2">
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold">Chamados</h1>
-          <Link href="/dashboard/new" className="bg-blue-500 px-4 py-1 rounded text-white">
+          <Link
+            href="/dashboard/new"
+            className="bg-blue-500 px-4 py-1 rounded text-white"
+          >
             Abrir chamado
           </Link>
         </div>
-
 
         <table className="min-w-full my-2">
           <thead>
             <tr>
               <th className="font-medium text-left pl-1">CLIENTE</th>
-              <th className="font-medium text-left hidden sm:block">DATA CADASTRO</th>
+              <th className="font-medium text-left hidden sm:block">
+                DATA CADASTRO
+              </th>
               <th className="font-medium text-left">STATUS</th>
               <th className="font-medium text-left">#</th>
             </tr>
           </thead>
           <tbody>
-
-            <TicketItem />
-
-            <TicketItem />
-
+            {tickets.map((ticket) => (
+              <TicketItem
+                key={ticket.id}
+                ticket={ticket}
+                customer={ticket.customer}
+              />
+            ))}
           </tbody>
         </table>
       </main>
     </Container>
-  )
+  );
 }
