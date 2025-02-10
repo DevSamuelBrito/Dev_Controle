e8da88164e5b2e31591b65d09ec1fba36df75205
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-02-10 17:21:33 -0300

    criando um botao de refresh

--- src/app/open/components/FormTicket/index.tsx
+++ src/app/open/components/FormTicket/index.tsx
@@ -10,7 +10,7 @@ interface FormTicketProps {
   customer: CustomerDataInfo;
 }
 
-export function FormTicket({customer}: FormTicketProps) {
+export function FormTicket({ customer }: FormTicketProps) {
   const schema = z.object({
     name: z.string().min(1, "O nome do chamado é obrigatório"),
     description: z.string().min(1, "Descreva um pouco sobre o seu problema..."),
@@ -27,17 +27,21 @@ export function FormTicket({customer}: FormTicketProps) {
     resolver: zodResolver(schema),
   });
 
-  async function handleRegister(data:FormData){
-    const response = await api.post("/api/ticket",{
+  async function handleRegister(data: FormData) {
+    const response = await api.post("/api/ticket", {
       name: data.name,
       description: data.description,
-      customerId: customer.id
-    })
-    console.log(response.data)
+      customerId: customer.id,
+    });
+    setValue("name", "");
+    setValue("description", "");
   }
 
   return (
-    <form className="bg-slate-200 mt-6 px-4 py-6 rounded border-2" onSubmit={handleSubmit(handleRegister)}>
+    <form
+      className="bg-slate-200 mt-6 px-4 py-6 rounded border-2"
+      onSubmit={handleSubmit(handleRegister)}
+    >
       <label className="mb-1 font-medium text-lg">Nome do Chamado:</label>
       <Input
         placeholder="Digite o nome do chamado"
@@ -54,7 +58,9 @@ export function FormTicket({customer}: FormTicketProps) {
         {...register("description")}
       ></textarea>
       {errors.description && (
-        <p className="bg-red-200 my-1 mt-1 mb-4">{errors.description.message}</p>
+        <p className="bg-red-200 my-1 mt-1 mb-4">
+          {errors.description.message}
+        </p>
       )}
       <button
         type="submit"
